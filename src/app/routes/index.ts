d51f49c13b37a34fc11ac7338acc38cad175fa91
shamim-5 <shamim.somadder5@gmail.com>
2023-08-20 02:50:30 +0600

    create prisma model for AcademicSemester, AcademicFaculty, AcademicDepartment, Faculty and Student. implemented modules for academicSemester whereuse createAsync for try/catch, validateRequest middleware for handle zod error file

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { AcademicSemesterRoute } from '../modules/academicSemester/academicSemester.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/academic-semester',
+    route: AcademicSemesterRoute,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
