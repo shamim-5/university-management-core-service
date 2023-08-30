34f6d69cd261059815303ab8ce3544fa69685bac
shamim-5 <shamim.somadder5@gmail.com>
2023-08-30 23:23:58 +0600

    building, course, faculty, student and room implemented

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import { AcademicDepartmentRoute } from '../modules/academicDepartment/academicDepartment.route';
 import { AcademicFacultyRoute } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicSemesterRoute } from '../modules/academicSemester/academicSemester.route';
+import { StudentRoute } from '../modules/student/student.route';
+import { CourseRoute } from '../modules/course/course.route';
+import { FacultyRoute } from '../modules/faculty/faculty.route';
+import { RoomRoute } from '../modules/room/room.route';
+import { BuildingRoute } from '../modules/building/building.route';
 
 const router = express.Router();
 
@@ -15,10 +20,30 @@ const moduleRoutes = [
     path: '/academic-faculty',
     route: AcademicFacultyRoute,
   },
-      {
-        path: '/academic-department',
-        route: AcademicDepartmentRoute,
-      },
+  {
+    path: '/academic-department',
+    route: AcademicDepartmentRoute,
+  },
+  {
+    path: '/student',
+    route: StudentRoute,
+  },
+  {
+    path: '/course',
+    route: CourseRoute,
+  },
+  {
+    path: '/faculty',
+    route: FacultyRoute,
+  },
+  {
+    path: '/room',
+    route: RoomRoute,
+  },
+  {
+    path: '/building',
+    route: BuildingRoute,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
